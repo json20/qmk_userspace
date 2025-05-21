041bde05d1f7d7a8b2170875359c473100525b15
Jason <66883665+json20@users.noreply.github.com>
2025-05-21 20:40:28 +0800

    add

--- keyboards/lily58/rev1/keymaps/lily58_via/keymap.c
+++ keyboards/lily58/rev1/keymaps/lily58_via/keymap.c
@@ -20,19 +20,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| Lower       Raise  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |   [  | /Enter  /       \Space \  |   ;  |BackSP| RGUI |
+ *                   | LAlt | LGUI |  BackSp  | /Enter  /       \Space \  |   ;  |  | RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
- [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_BSLS, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   TG(_LOWER), TG(_RAISE),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                    KC_LALT, KC_LGUI, TG(_SYMBOLS),    KC_ENT,     KC_SPC,                KC_SCLN, KC_BSPC, KC_RGUI
+[_QWERTY] = LAYOUT(
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                    KC_6,    KC_7,       KC_8,    KC_9,    KC_0,    KC_EQL,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_MINS,
+  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                    KC_H,    KC_J,       KC_K,    KC_L,    KC_BSLS, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,      MO(_RAISE),   MO(_SYMBOLS),   KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                             KC_LALT, KC_LGUI, KC_BSPC,   KC_ENT,       KC_SPC,         KC_SCLN, MO(_LOWER), KC_RGUI
 ),
-
 
 [_SYMBOLS] = LAYOUT(
   _______,  KC_LT,   KC_GT,   KC_MINS, KC_PIPE, KC_BSLS,                      KC_CIRC, KC_AT,   KC_AMPR, KC_DLR,  KC_RIGHT, KC_EQL,
@@ -43,13 +42,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
-  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______,KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_EQL,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                             KC_7,    KC_8,    KC_9,    KC_O,    KC_P,    KC_MINS,
+  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                             KC_4,    KC_5,    KC_6,    KC_L,    KC_BSLS, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(_LOWER), MO(_SYMBOLS),   KC_1,    KC_2,    KC_3,    KC_DOT,  KC_SLSH, KC_RSFT,
+                    KC_LALT, KC_LGUI, MO(_RAISE),    KC_ENT,     KC_SPC,                  KC_SCLN, KC_BSPC, KC_RGUI
 ),
-
 
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,
