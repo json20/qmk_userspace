d00bd2cbd04c5d2afceb466374ede158ea61fe3d
Jason <66883665+json20@users.noreply.github.com>
2025-05-23 11:14:28 +0800

    add

--- keyboards/lily58/rev1/keymaps/lily58_via/keymap.c
+++ keyboards/lily58/rev1/keymaps/lily58_via/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                    KC_H,    KC_J,       KC_K,    KC_L,    KC_BSLS, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,      MO(_RAISE),   MO(_SYMBOLS),   KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                             KC_LALT, KC_LGUI, KC_BSPC,   KC_ENT,       KC_SPC,         KC_SCLN, MO(_LOWER), KC_RGUI
+                             KC_LALT, KC_LGUI, KC_SCLN,   KC_ENT,       KC_SPC,         KC_BSPC, MO(_LOWER), KC_RGUI
 ),
 
 [_SYMBOLS] = LAYOUT(
